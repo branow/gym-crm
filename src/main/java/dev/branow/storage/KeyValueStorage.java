06aadd240059461e193ac5bb3ea0b8c52def4899
branow <branow.code@gmail.com>
2025-02-12 02:05:11 +0200

    Remove unused class to maintain a cleaner codebase

--- src/main/java/dev/branow/storage/KeyValueStorage.java
+++
@@ -1,9 +0,0 @@
-package dev.branow.storage;
-
-import java.util.Optional;
-
-public interface KeyValueStorage {
-    <T> Optional<T> get(String key, Reference<T> reference);
-    <T> void save(String key, T value);
-}
-
--- src/main/java/dev/branow/storage/Storage.java
+++
@@ -1,8 +0,0 @@
-package dev.branow.storage;
-
-import java.io.InputStream;
-
-public interface Storage {
-    InputStream read();
-    void write(InputStream stream);
-}
